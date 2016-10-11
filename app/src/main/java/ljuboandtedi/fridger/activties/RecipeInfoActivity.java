1427f829cecc25c8eedd39660ed0522848940a33
TeodorDimitrovGH <nolightnosound@gmail.com>
2016-10-11 03:38:02 +0300

    Added relatedMeals recycleView in the sliding part of the recipe info,it is not showing without reason, to be fixed tomorrow morning.

--- app/src/main/java/ljuboandtedi/fridger/activties/RecipeInfoActivity.java
+++ app/src/main/java/ljuboandtedi/fridger/activties/RecipeInfoActivity.java
@@ -25,17 +25,23 @@ import android.widget.Toast;
 
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 import ljuboandtedi.fridger.R;
 import ljuboandtedi.fridger.adapters.IngredientsRecyclerAdapter;
+import ljuboandtedi.fridger.adapters.MealRecyclerAdapter;
 import ljuboandtedi.fridger.model.DatabaseHelper;
 import ljuboandtedi.fridger.model.Meal;
 import ljuboandtedi.fridger.model.MealManager;
@@ -63,6 +69,7 @@ public class RecipeInfoActivity extends DrawerActivity {
     RecyclerView ingredientsList;
     IngredientsRecyclerAdapter adapter;
     TextView continueExploring;
+    RecyclerView relatedMeals;
     private SlidingUpPanelLayout slidingLayout;
 
     Recipe recipe;
@@ -71,6 +78,10 @@ public class RecipeInfoActivity extends DrawerActivity {
         super.onCreate(savedInstanceState);
         super.replaceContentLayout(R.layout.activity_recipe_info_slidingbar, super.CONTENT_LAYOUT_ID);
         iv = (ImageView) findViewById(R.id.recipe_info_Image);
+
+        relatedMeals = (RecyclerView) findViewById(R.id.recycleListForRelatedMeals);
+        relatedMeals.setLayoutManager(new LinearLayoutManager(RecipeInfoActivity.this,LinearLayoutManager.HORIZONTAL,false));
+        new RequestTaskForRelatedMeals().execute("http://api.yummly.com/v1/api/recipes?_app_id=19ff7314&_app_key=8bdb64c8c177c7e770c8ce0d000263fd&q=&maxResult=10&start=10");
 
         course1TV = (TextView) findViewById(R.id.recipe_info_course1);
         course2TV = (TextView) findViewById(R.id.recipe_info_course2);
@@ -204,7 +215,6 @@ public class RecipeInfoActivity extends DrawerActivity {
 
         buyALlButton.setText("Buy All");
         adapter = new IngredientsRecyclerAdapter(this,recipe.getIngredientLines());
-        final int sizeOfSList = DatabaseHelper.getInstance(RecipeInfoActivity.this).getUserShoppingList(DatabaseHelper.getInstance(RecipeInfoActivity.this).getCurrentUser().getFacebookID()).size();
 
         buyALlButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -233,6 +243,8 @@ public class RecipeInfoActivity extends DrawerActivity {
 
             }
         });
+
+
         ingredientsList = (RecyclerView) findViewById(R.id.recycleListForIngredients);
         ingredientsList.setLayoutManager(new LinearLayoutManager(this));
         ingredientsList.setAdapter(adapter);
@@ -322,5 +334,112 @@ public class RecipeInfoActivity extends DrawerActivity {
 
         }
     }
+    class RequestTaskForRelatedMeals extends AsyncTask<String, Void, String> {
 
+        @Override
+        protected String doInBackground(String... params) {
+            String address = params[0];
+            String json = "";
+            try {
+                URL url = new URL(address);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+                Scanner sc = new Scanner(connection.getInputStream());
+                while(sc.hasNextLine()){
+                    json+=(sc.nextLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(String jsonX) {
+            List<String> recipes = new ArrayList<>();
+            try {
+                JSONObject json = new JSONObject(jsonX);
+                JSONArray matches = json.getJSONArray("matches");
+
+
+                //Takes the first JOBJ in matches!!!
+                //Atm it takes the first Soup found in the search of soups.
+                //It will be in a recycle view and it will be in an array so we can take all the soups. This is sample for the first soup.
+
+
+                //Lets try with array
+                for (int i = 0; i < matches.length(); i++) {
+                    Log.i("matches", matches.length() + "");
+                    String ingredientsString = "";
+                    String recipeName = "";
+                    String flavorsString = "";
+                    String sourceOfInfo = "";
+                    String picUrl = "";
+                    int ratingInt = 0;
+                    String attributes = matches.getString(i);
+                    JSONObject attributesInJson = new JSONObject(attributes);
+
+                    //Takes json array for ingredients
+                    if (!attributesInJson.isNull("ingredients")) {
+                        JSONArray ingredientsInJson = attributesInJson.getJSONArray("ingredients");
+                        final int resultsLength = ingredientsInJson.length();
+                        for (int j = 0; j < resultsLength; j++) {
+                            //Appending String (Should be stringBuilder) to get info for all ingredients
+                            ingredientsString += ingredientsInJson.getString(j) + ", ";
+
+                        }
+                    }
+                    //recipeName in yummy for the meal
+                    if (!attributesInJson.isNull("recipeName")) {
+                        recipeName = attributesInJson.getString("recipeName");
+                    }
+                    if (!attributesInJson.isNull("sourceDisplayName")) {
+                        sourceOfInfo = attributesInJson.getString("sourceDisplayName");
+                    }
+                    //Rating of the recipe
+                    if (!attributesInJson.isNull("rating")) {
+                        ratingInt = attributesInJson.getInt("rating");
+                    }
+                    Log.i("flavors", i + "");
+
+                    String crappyPrefix = "null";
+                    if (attributes.startsWith("null")) {
+                        attributes = attributes.substring(crappyPrefix.length(), attributes.length());
+                    }
+
+                    if (!attributesInJson.isNull("flavors")) {
+
+                        JSONObject flavorsInJson = attributesInJson.getJSONObject("flavors");
+                        //Appending String (Should be stringBuilder) to get info for all flavors
+                        flavorsString += "piquant: " + flavorsInJson.getDouble("piquant");
+                        flavorsString += "\nmeaty: " + flavorsInJson.getDouble("meaty");
+                        flavorsString += "\nbitter: " + flavorsInJson.getDouble("bitter");
+                        flavorsString += "\nsweet: " + flavorsInJson.getDouble("sweet");
+                        flavorsString += "\nsour: " + flavorsInJson.getDouble("sour");
+                        flavorsString += "\nsalty: " + flavorsInJson.getDouble("salty");
+                    }
+                    String id = "";
+                    if (!attributesInJson.isNull("id")) {
+                        //JSONObject aboutPicture = attributesInJson.getJSONObject("id");
+                        id = attributesInJson.getString("id");
+
+                        Log.e("ID", id);
+                    }
+
+                    recipes.add(id);
+                Log.e("receptite",recipes.toString());
+                }
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            relatedMeals.setAdapter(new MealRecyclerAdapter(RecipeInfoActivity.this,recipes));
+            Log.e("bqhtuk","123");
+
+        }
+
+    }
 }
